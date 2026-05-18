d88deb580b51f4d76369f2a5a4c002475941f020
AsilahMJ <71244700+AsilahMJ@users.noreply.github.com>
2026-05-18 12:46:01 -0700

    Updating enemy pathfinding

---
+++ Assets/Scripts/EnemyScripts/Enemy AI.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Pathfinding; 
+
+public class EnemyAI : MonoBehaviour
+{
+    Transform destination; 
+    AIPath ai;
+
+    void Awake()
+    {
+        ai = GetComponent<AIPath>();
+
+        
+        GameObject playerObject = GameObject.Find("Player");
+        
+        if (playerObject != null)
+        {
+            destination = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("EnemyAI: Could not find a GameObject named 'Player' in the scene!");
+        }
+    }
+
+    void Update()
+    {   
+        
+        if (ai != null && destination != null) 
+        {
+            ai.destination = destination.position;
+        }
+    }
+}
