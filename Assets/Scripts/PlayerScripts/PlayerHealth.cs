47abc5d8dcfd1517d7be7f186538ad689c0b9acc
lnhuangg <lesliehuang@g.ucla.edu>
2026-06-01 18:13:46 -0700

    add death screen and scene changes

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -25,8 +26,8 @@ public class PlayerHealth : MonoBehaviour
 
     void PDie()
     {
+        SceneManager.LoadScene("DeathScreen");
         Debug.Log("RipPlayer");
-        Destroy(gameObject);
     }
 
 }
